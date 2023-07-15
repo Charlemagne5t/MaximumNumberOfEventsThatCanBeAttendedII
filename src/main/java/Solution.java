4efc47b8e87184fdf3d387eced8f3cbd0bbba4d0
alek <wait82985@gmail.com>
2023-07-15 15:32:37 +0600

    problem solved

--- src/main/java/Solution.java
+++ src/main/java/Solution.java
@@ -1,26 +1,30 @@
-import java.util.Arrays;
-import java.util.Comparator;
+import java.util.*;
 
 public class Solution {
     public int maxValue(int[][] events, int k) {
         Arrays.sort(events, Comparator.comparingInt(a -> a[0]));
-        return dfs(events, 0, k, 0);
+        Map<String, Integer> memo = new HashMap<>();
+        return dfs(events, 0, k, 0, memo);
 
     }
 
-    private int dfs(int[][] events, int index, int k, int currentDay) {
+    private int dfs(int[][] events, int index, int k, int currentDay, Map<String, Integer> memo) {
         if (index == events.length || k == 0) {
             return 0;
+        }
+        if (memo.containsKey(index + " " + k + " " + currentDay)) {
+            return memo.get(index + " " + k + " " + currentDay);
         }
 
         int result = 0;
         int take = 0;
         if (events[index][0] >= currentDay) {
-            take = events[index][2] + dfs(events, index + 1, k - 1, events[index][1] + 1);
+            take = events[index][2] + dfs(events, index + 1, k - 1, events[index][1] + 1, memo);
         }
-        int doNothing = dfs(events, index + 1, k, currentDay);
+        int doNothing = dfs(events, index + 1, k, currentDay, memo);
         result = Math.max(take, doNothing);
 
+        memo.put(index + " " + k + " " + currentDay, result);
         return result;
     }
 }
